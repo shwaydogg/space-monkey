0e3f86180c7c89644275129cf213ce776dd467e3
Gregory Schwedock <schwedock@gmail.com>
2016-03-15 17:57:00 -0400

    Added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Space Monkey brings the magic of Astronomer to the server, adds more automatic features and bypasses ad-blockers.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/shwaydogg/space-monkey',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
